b0ec3d7a32ff4277420aa3617aa59a3b11c78ea0
73-hana <hanaprgrm1997@gmail.com>
2023-04-08 22:13:36 +0900

    add nuxtjs/robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
+    ['@nuxtjs/robots', {UserAgent: '*', Disallow: '/'}],
   ],
   css: [
     {
